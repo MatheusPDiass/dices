6de1c989bec33ee1a6a6a95c126644fe67f6a46c
MatheusPDiass <107899419+MatheusPDiass@users.noreply.github.com>
2024-07-03 16:45:35 -0300

    code update - button function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import randonResult from "./dices.js";
+
 function changeDice(value) {
   const dicesArea = document.getElementById("dicesArea");
   dicesArea.classList = "";
@@ -9,8 +11,14 @@ function changeDice(value) {
     case 6:
       dicesArea.classList = "dices-6";
       break;
+    case 8:
+      dicesArea.classList = "dices-8";
+      break;
     case 10:
       dicesArea.classList = "dices-10";
+      break;
+    case 12:
+      dicesArea.classList = "dices-12";
       break;
     case 20:
       dicesArea.classList = "dices-20";
@@ -37,9 +45,13 @@ function renderResult(number) {
   dicesArea.appendChild(resultNumber);
 }
 
-function randonDice(value) {
-  console.log(value);
-}
+const buttons = document.querySelectorAll(".randonDice");
 
-let newDice = document.getElementById("randonDice");
-newDice.addEventListener("click", randonDice(newDice.value));
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const value = parseInt(this.value);
+    changeDice(value);
+    const number = randonResult(value);
+    renderResult(number);
+  });
+});
